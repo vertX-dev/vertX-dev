e0701ca8df2343432579db31ce71c8c10c7b71b3
vertX-dev <danasolovej8@gmail.com>
2025-05-08 12:03:06 +0300

    Create Practice7and9.cpp

---
+++ Practice/Practice7and9.cpp
@@ -0,0 +1,122 @@
+// ConsoleApplication8.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void randomArray() {
+	const int rows = 4;
+	const int cols = 5;
+	int array[rows][cols];
+
+	srand(time(0));
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			array[i][j] = rand() % 11 - 5;
+		}
+	}
+
+	cout << "Array:\n";
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			cout << array[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	int sum = array[0][2] + array[2][2];
+	cout << "The sum of 1st and 3rd elements of the third column: " << sum << endl;
+}
+void specificArray() {
+	const int rows = 12;
+	const int cols = 10;
+	int array[rows][cols];
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			array[i][j] = (j % 2 == 0) ? (12 * (j + 1) + i - 11) : (12 * (j + 1) - i);
+		}
+	}
+
+	cout << "Array:\n";
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			cout << array[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+void switchHalfsOfArray() {
+	srand(time(0));
+	const int rows = 8;//even; positive; integer
+	const int cols = 10;//positive; integer
+	int array[rows][cols];
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			array[i][j] = rand() % 2000 - 1000;
+		}
+	}
+	cout << "Starting Array:\n";
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			cout << array[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	//Switching process
+	int temp;
+	for (int i = 0; i < (rows / 2); i++) {
+		for (int j = 0; j < cols; j++) {
+			temp = array[i][j];
+			array[i][j] = array[rows - i - 1][j];
+			array[rows - i - 1][j] = temp;
+		}
+	}
+	cout << "Switched Array:\n";
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			cout << array[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+//Practice 9
+bool isSortedAscending(int arr[], int size) {
+	for (int i = 1; i < size; ++i) {
+		if (arr[i] < arr[i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+void dynamicArray() {
+	int length = 15;
+	int *arr = new int[length];
+	for (int i = 0; i < length; ++i) {
+		arr[i] = rand() % 2000 - 1000;
+	}
+	cout << "Random array:\n";
+	for (int i = 0; i < length; ++i) {
+		cout << arr[i]<<"  ";
+	}
+	if (isSortedAscending(arr, length)) {
+		cout << "Sorted\n";
+	}
+	else {
+		cout << "Not sorted\n";
+	}
+	
+}
+
+int main() {
+	randomArray();
+	specificArray();
+	switchHalfsOfArray();
+	dynamicArray();
+	return 0;
+}
+
