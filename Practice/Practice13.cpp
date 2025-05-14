512d2819eff96b7310cb6f7080f9bdca1402aa80
vertX-dev <danasolovej8@gmail.com>
2025-05-14 10:17:19 +0300

    Update Practice13.cpp

--- Practice/Practice13.cpp
+++ Practice/Practice13.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,79 +6,84 @@
 using namespace std;
 
 struct STUDENT {
-    string lastNameInitials;
-    string groupNumber;
-    int grades[5];
+	string lastName;
+	string initials;
+	string groupNumber;
+	int grades[5];
 };
 
 const int NUM_STUDENTS = 5;
 
 // Function to calculate average grade
 double averageGrade(const STUDENT& student) {
-    int sum = 0;
-    for (int i = 0; i < 5; ++i) {
-        sum += student.grades[i];
-    }
-    return sum / 5.0;
+	int sum = 0;
+	for (int i = 0; i < 5; ++i) {
+		sum += student.grades[i];
+	}
+	return sum / 5.0;
 }
 
 int main() {
-    STUDENT students[NUM_STUDENTS];
+	STUDENT students[NUM_STUDENTS];
 
-    // Input data from keyboard
-    cout << "Enter data for 5 students:\n";
-    for (int i = 0; i < NUM_STUDENTS; ++i) {
-        cout << "Student " << i + 1 << ":\n";
-        cout << "Last name and initials: ";
-        cin.ignore();
-        getline(cin, students[i].lastNameInitials);
-        cout << "Group number: ";
-        getline(cin, students[i].groupNumber);
-        cout << "Enter 5 grades: ";
-        for (int j = 0; j < 5; ++j) {
-            cin >> students[i].grades[j];
-        }
-    }
+	// Input data from keyboard
+	cout << "Enter data for 5 students:\n";
+	for (int i = 0; i < NUM_STUDENTS; ++i) {
+		cout << "Student " << i + 1 << ":\n";
+		cout << "Last name: ";
+		cin.ignore();
+		getline(cin, students[i].lastName);
+		cout << "Initials: ";
+		getline(cin, students[i].initials);
+		cout << "Group number: ";
+		getline(cin, students[i].groupNumber);
+		cout << "Enter 5 grades: ";
+		for (int j = 0; j < 5; ++j) {
+			cin >> students[i].grades[j];
+		}
+	}
 
-    // Write to file
-    ofstream outFile("students.txt");
-    for (int i = 0; i < NUM_STUDENTS; ++i) {
-        outFile << students[i].lastNameInitials << "\n"
-                << students[i].groupNumber << "\n";
-        for (int j = 0; j < 5; ++j) {
-            outFile << students[i].grades[j] << " ";
-        }
-        outFile << "\n";
-    }
-    outFile.close();
+	// Write to file
+	ofstream outFile("students.txt");
+	for (int i = 0; i < NUM_STUDENTS; ++i) {
+		outFile << students[i].lastName << "\n"
+			<< students[i].initials << "\n"
+			<< students[i].groupNumber << "\n";
+		for (int j = 0; j < 5; ++j) {
+			outFile << students[i].grades[j] << " ";
+		}
+		outFile << "\n";
+	}
+	outFile.close();
 
-    // Read from file
-    ifstream inFile("students.txt");
-    STUDENT readStudents[NUM_STUDENTS];
-    for (int i = 0; i < NUM_STUDENTS; ++i) {
-        getline(inFile, readStudents[i].lastNameInitials);
-        getline(inFile, readStudents[i].groupNumber);
-        for (int j = 0; j < 5; ++j) {
-            inFile >> readStudents[i].grades[j];
-        }
-        inFile.ignore(); // skip newline after grades
-    }
-    inFile.close();
+	// Read from file
+	ifstream inFile("students.txt");
+	STUDENT readStudents[NUM_STUDENTS];
+	for (int i = 0; i < NUM_STUDENTS; ++i) {
+		getline(inFile, readStudents[i].lastName);
+		getline(inFile, readStudents[i].initials);
+		getline(inFile, readStudents[i].groupNumber);
+		for (int j = 0; j < 5; ++j) {
+			inFile >> readStudents[i].grades[j];
+		}
+		inFile.ignore(); // skip newline after grades
+	}
+	inFile.close();
 
-    // Display students with average > 4.0
-    bool found = false;
-    cout << "\nStudents with average grade > 4.0:\n";
-    for (int i = 0; i < NUM_STUDENTS; ++i) {
-        if (averageGrade(readStudents[i]) > 4.0) {
-            cout << "Name: " << readStudents[i].lastNameInitials
-                 << ", Group: " << readStudents[i].groupNumber << "\n";
-            found = true;
-        }
-    }
+	// Display students with average > 4.0
+	bool found = false;
+	cout << "\nStudents with average grade > 4.0:\n";
+	for (int i = 0; i < NUM_STUDENTS; ++i) {
+		if (averageGrade(readStudents[i]) > 4.0) {
+			cout << "Grade: " << averageGrade << " Last name: " << readStudents[i].lastName
+				<< ", Group: " << readStudents[i].groupNumber << "\n";
+			found = true;
+		}
+	}
 
-    if (!found) {
-        cout << "No students with average grade > 4.0\n";
-    }
+	if (!found) {
+		cout << "No students with average grade > 4.0\n";
+	}
 
-    return 0;
+	return 0;
 }
